1d5b05b02363fd7832e397ca526ba938a6e4f6f2
tytech3 <48813565+tytech3@users.noreply.github.com>
2020-04-22 22:45:42 -0700

    Init
    init

--- CrossyWordz/ContentView.swift
+++ CrossyWordz/ContentView.swift
@@ -9,13 +9,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    init() {
+        //TEST DATA
+        let row1 = [textCell(character: "T", tag:1), textCell(character: "A", tag:2), textCell(character: "B", tag:3), textCell(character: "", tag:4), textCell(character: "", tag:5)]
+        let row2 = [textCell(character: "T", tag:6), textCell(character: "A", tag:7), textCell(character: "B", tag:8), textCell(character: "", tag:9), textCell(character: "", tag:10)]
+        let row3 = [textCell(character: "T", tag:11), textCell(character: "A", tag:12), textCell(character: "B", tag:13), textCell(character: "", tag:14), textCell(character: "", tag:15)]
+        let row4 = [textCell(character: "T", tag:16), textCell(character: "A", tag:17), textCell(character: "B", tag:18), textCell(character: "", tag:19), textCell(character: "", tag:20)]
+        _matrix = State(initialValue: [row1, row2, row3, row4])
+    }
+    
+    @State var matrix: [[textCell]]
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .leading){
+            HStack(alignment: .top, spacing: CGFloat(0.5)){
+                ForEach(0...matrix.count, id: \.self) {
+                    self.matrix[0][$0]
+                }
+            }
+            HStack(alignment: .top, spacing: CGFloat(0.5)){
+                ForEach(0...matrix.count, id: \.self) {
+                    self.matrix[1][$0]
+                }
+            }
+            HStack(alignment: .top, spacing: CGFloat(0.5)){
+                ForEach(0...matrix.count, id: \.self) {
+                    self.matrix[2][$0]
+                }
+            }
+            HStack(alignment: .top, spacing: CGFloat(0.5)){
+                ForEach(0...matrix.count, id: \.self) {
+                    self.matrix[3][$0]
+                }
+            }
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+
+struct textCell: View {
+    
+    init(character: String, tag: Int) {
+        _character = State(initialValue: character)
+        _tag = State(initialValue: tag)
+    }
+    
+    @State var character = ""
+    @State var userInput = ""
+    @State var tag: Int
+    let characterLimit = 1
+    
+    
+    
+    var body: some View {
+        SATextField(tag: self.tag, placeholder: "", changeHandler: { (newString) in
+            if(newString.count > 1){
+                
+            }
+        }, onCommitHandler: {
+            print("done!")
+        })
+            .frame(width: 65, height: 50, alignment: .center)
+            .background(Color.blue)
+            .foregroundColor(Color.white)
+            .multilineTextAlignment(.center)
     }
 }
+
